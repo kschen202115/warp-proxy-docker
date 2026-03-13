5c230ce6f6250584012985995469a634d4aeaf2d
kschen202115 <124901446+kschen202115@users.noreply.github.com>
2026-03-13 22:13:20 +0800

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,12 +2,7 @@
 echo "[INFO] Running healthcheck..."
 IP=$(curl -s --socks5 127.0.0.1:1080 --connect-timeout 10 https://cloudflare.com/cdn-cgi/trace | grep "ip=" | cut -d'=' -f2)
 
-if [ -z "$IP" ]; then
-   echo "[ERROR] Proxy dead or unreachable, restarting wireproxy..."
-   pkill wireproxy
-   sleep 3
-   cd /etc/warp-go
-   wireproxy -c warp.conf > /var/log/wireproxy.log 2>&1 &
-else
-   echo "[INFO] Healthcheck passed. Current IP: $IP"
-fi
+sleep 3
+
+echo "[INFO] Healthcheck passed. Current IP: $IP"
+
